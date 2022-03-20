1cafc842e44777a26fb4a0cc574fdbdb186c6912
Coder731 <stephenobrien1@gmail.com>
2022-03-20 01:46:55 +0000

    settings py Update INSTALLED APPS Add YourThoughts

--- Profile_Project_4/settings.py
+++ Profile_Project_4/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'todo',  # added per step in part 1 #4
-
+    'Your_Thoughts'
 ]
 
 MIDDLEWARE = [
