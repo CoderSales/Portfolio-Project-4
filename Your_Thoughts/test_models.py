8cae3d15ff050f30e321d4a6e340e60be211de8c
Coder731 <stephenobrien1@gmail.com>
2022-03-21 22:49:19 +0000

    Add test to test models py

---
+++ Your_Thoughts/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .models import Post
+
+
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        post = Post.objects.create(name='Test Post')
+        self.assertFalse(post.done)
