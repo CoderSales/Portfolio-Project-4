8574302d7837cfad3e041e0760131da1952a6751
Coder731 <stephenobrien1@gmail.com>
2022-03-30 15:59:34 +0000

    Update app urls py urlpatterns

--- Your_Thoughts/urls.py
+++ Your_Thoughts/urls.py
@@ -1,9 +1,36 @@
 from . import views
-from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+# from Your_Thoughts import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+
+    # path('', views.get_comment, name='get_comment'),
+
+    # path('add', views.add_post, name='add'),
+    # path('edit/<post_id>', views.edit_post, name='edit'),
+    # path('toggle/<post_id>', views.toggle_post, name='toggle'),
+    # path('delete/<post_id>', views.delete_post, name='delete'),
+    # path('summernote/', include('django_summernote.urls')),
+    # path('', include('Your_Thoughts.urls'), name='Your_Thoughts_urls'),
+    # path('accounts/', include('allauth.urls')),
+]
+
+urlpatterns = [
+    # cannot seemingly have two roots, so line not working but page loads
+    # path('', include('Your_Thoughts.urls'), name='Your_Thoughts_urls'),
+
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+    path('', views.get_comment, name='get_comment'),
+    path('add', views.add_post, name='add'),
+
+    path('edit/<post_id>', views.edit_post, name='edit'),
+    path('toggle/<post_id>', views.toggle_post, name='toggle'),
+    path('delete/<post_id>', views.delete_post, name='delete'),
+    path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
+    # path('admin/', admin.site.urls),
+    # path('add', views.add_post, name='add'),
 ]
--- Your_Thoughts/views.py
+++ Your_Thoughts/views.py
@@ -8,63 +8,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 # from .models import Post
 
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 6
-
-class PostDetail(View):
-
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('-created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": False,
-                "liked": liked,
-                "comment_form": CommentForm()
-            },
-        )
-
-    def post(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('-created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
-
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked,
-                "commented": True,
-                "comment_form": CommentForm(),
-            },
-        )
+#  moved up functions before classes so not crashing (urls.py Your_Thoughts)
 
 def get_comment(user_request):
     """
@@ -125,7 +69,6 @@ def toggle_post(user_request, post_id):
     post.save()
     return redirect('get_comment')
 
-
 def delete_post(user_request, post_id):
     """
     add delete button for post
@@ -134,6 +77,74 @@ def delete_post(user_request, post_id):
     post = get_object_or_404(Post, id=post_id)
     post.delete()
     return redirect('get_comment')
+
+# new function:
+def index(request):
+    """ A view to return the index page """
+    return render(request, 'Your_Thoughts/index.html')
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "commented": True,
+                "comment_form": CommentForm(),
+            },
+        )
+
+
+
+
+
 
 
 class PostLike(View):
@@ -147,3 +158,4 @@ class PostLike(View):
             post.likes.add(request.user)
         
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
