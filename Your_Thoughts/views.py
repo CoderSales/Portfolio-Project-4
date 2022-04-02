93cec12b8740d19f674df9d8a8a11f23b9cbc985
Coder731 <stephenobrien1@gmail.com>
2022-04-02 23:19:29 +0000

    Debug Posts not showing on user home page

--- Your_Thoughts/views.py
+++ Your_Thoughts/views.py
@@ -21,7 +21,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('-created_on')
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -34,17 +34,21 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": False,
                 "liked": liked,
-                "comment_form": comment_form,
+                "comment_form": CommentForm(),
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('-created_on')
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
+        # else:
+            # post.likes.add(request.user)
+
+        # return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
