0489db5c993219ffa26727a0b60436c4551433c6
Coder731 <stephenobrien1@gmail.com>
2022-04-04 02:45:23 +0000

    Delete commented code

--- Profile_Project_4/urls.py
+++ Profile_Project_4/urls.py
@@ -19,19 +19,9 @@ https://stackoverflow.com/questions/11439447/django-import-views-from-separate-a
 from django.contrib import admin
 from django.urls import path, include
 from Your_Thoughts import views
-# # from home.views import index as index1
-# from Your_Thoughts.views import index as index2
-# # from Profile_Project_4.templates import index as indexProj
-# from django.conf import settings
-# from django.conf.urls.static import static
-# # from . import views
-# # import home
-# # import home
-# # from home import products as products1
 
 
 urlpatterns = [
-    # #1 path('admin/', home.admin.site.urls),
     path('admin/', admin.site.urls),
     path('add', views.add_post, name='add'),
     path('edit/<post_id>', views.edit_post, name='edit'),
@@ -40,20 +30,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('Your_Thoughts.urls'), name='Your_Thoughts_urls'),
     path('accounts/', include('allauth.urls')),
-    # #2 path('accounts/', home.include('allauth.urls')),
-
-    # # path('althome2', views.index, name='home'),
-    # # path('', views.index, name='Your_Thoughts'), # duplicated from above
-    # #3 path('', home.views.index, name='home.urls'),
-    # path('', index1, name='home.urls'),
-    # path('', index2, name='Your_Thoughts.urls'),
-    # path('', views.index, name='Your_Thoughts.urls'),
-
-
-    # path('', views.get_comment, name='get_comment'),
-    # path('option2', indexProj.get_comment, name='get_comment'),
-
-    # path('products/', include('products.urls')),
 
 ]
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
